f1cf7c0e86601ac003f5cd6f5689ff9ef7917a9e
Andy Rose <thingswithhandles@gmail.com>
2020-03-06 11:12:29 -0600

    feat(core): add keycodes and modifiers

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/common-behaviors/constructor';
 export * from './lib/common-behaviors/disabled';
 export * from './lib/navigation/nav-link';
+export * from './lib/keycodes/keycodes';
+export * from './lib/keycodes/modifiers';
