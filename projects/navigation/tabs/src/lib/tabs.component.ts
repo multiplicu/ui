9df20feef02bf8f2be2b2b7b566ba4b2ba857022
Andy Rose <thingswithhandles@gmail.com>
2020-03-11 08:40:41 -0500

    feat(tabs): initial tabs library

---
+++ projects/navigation/tabs/src/lib/tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'xcu-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsComponent implements OnInit {
+  public constructor() {}
+
+  public ngOnInit(): void {}
+}
