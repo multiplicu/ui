92a521effc2f3d34593ffd7e32909f88ebcce956
Andy Rose <thingswithhandles@gmail.com>
2020-04-06 08:59:21 -0500

    refactor(tabs): add exportAs to tabs

--- projects/navigation/tabs/src/lib/tabs.component.ts
+++ projects/navigation/tabs/src/lib/tabs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'xcu-tabs',
+  exportAs: 'xcuTabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss'],
 })
