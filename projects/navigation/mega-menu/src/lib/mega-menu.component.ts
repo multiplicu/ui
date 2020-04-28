a955efe857fcbead9b9c86b4a801fa6e9683fc0f
Andy Rose <thingswithhandles@gmail.com>
2020-04-28 12:16:13 -0500

    feat(mega-menu): menu structure and styling for whole menu

--- projects/navigation/mega-menu/src/lib/mega-menu.component.ts
+++ projects/navigation/mega-menu/src/lib/mega-menu.component.ts
@@ -1,11 +1,11 @@
-import { NavLink } from '@multiplicu/ui/core';
 import {
-  Component,
-  OnInit,
   ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
   Input,
-  EventEmitter,
 } from '@angular/core';
+import { NavLink } from '@multiplicu/ui/core';
 
 @Component({
   selector: `xcu-mega-menu, div[xcu-mega-menu], ul[xcu-mega-menu], nav[xcu-mega-menu]`,
@@ -14,12 +14,17 @@ import {
   styleUrls: ['./mega-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MegaMenuComponent implements OnInit {
+export class MegaMenuComponent {
+  @HostBinding('class.xcu-mega-menu--active')
   @Input()
   public isActive: boolean;
+
   @Input() public link: NavLink;
 
-  public constructor() {}
-
-  public ngOnInit(): void {}
+  public constructor(public elementRef: ElementRef) {
+    // Add a class that applies to all mega menus. This makes it easier to target if somebody
+    // wants to target all mega menus. We do it here rather than `host` to ensure that
+    // the class is applied to derived classes.
+    elementRef.nativeElement.classList.add('xcu-mega-menu');
+  }
 }
