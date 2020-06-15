3997995211e72a73999e252347ae821e5ab681a6
Andy Rose <thingswithhandles@gmail.com>
2020-06-15 15:35:01 -0500

    chore(deps): add scripts to build all packages

---
+++ scripts/build_all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+ng build core &&
+# Copy Core output to be used in other projects
+npm run copyDist &&
+### AVATAR ###
+ng build avatar &&
+### BADGE ###
+ng build badge &&
+### BUTTON ###
+ng build button &&
+### FAQ ###
+ng build faq &&
+### FORM ###
+ng build form-field &&
+ng build input &&
+### NAVIGATION ###
+ng build nav-toggle &&
+# Copy Toggle to be used in other projects
+npm run copyDist &&
+ng build switcher &&
+# Copy Switcher to be used in other projects
+npm run copyDist &&
+ng build tabs &&
+ng build local-nav &&
+ng build mega-menu &&
+ng build page-nav &&
+### SNACKBAR ###
+ng build snackbar
