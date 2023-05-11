870a5a995bc5536461829c08fe2edca4b9f72042
Andy Rose <andyrose@hey.com>
2023-05-11 10:23:20 -0500

    Revert "Angular CLI update for packages - @angular/core@next, @angular/cli@next, @angular/forms@next"
    This reverts commit 3cd4175a648b3c37aded33e2f40dac758351742e.

--- projects/form/input/src/lib/input.component.ts
+++ projects/form/input/src/lib/input.component.ts
@@ -3,18 +3,11 @@ import {
   Component,
   DoCheck,
   ElementRef,
-  Inject,
   Input,
-  NgZone,
   Optional,
   Self,
 } from '@angular/core';
-import {
-  FormGroupDirective,
-  NgControl,
-  NgForm,
-  Validators,
-} from '@angular/forms';
+import { FormGroupDirective, NgControl, NgForm } from '@angular/forms';
 import {
   CanUpdateErrorState,
   CanUpdateErrorStateCtor,
@@ -22,13 +15,8 @@ import {
   ErrorStateMatcher,
   getSupportedInputTypes,
   mixinErrorState,
-  Platform,
 } from '@multiplicu/ui/core';
-import {
-  XcuFormFieldComponent,
-  XcuFormFieldControl,
-  XCU_FORM_FIELD,
-} from '@multiplicu/ui/form/form-field';
+import { XcuFormFieldControl } from '@multiplicu/ui/form/form-field';
 import { Subject } from 'rxjs';
 
 let nextUniqueId: number = 0;
@@ -57,16 +45,13 @@ const _XcuInputMixinBase: CanUpdateErrorStateCtor & typeof XcuInputBase =
     '[attr.id]': 'id',
     '[disabled]': 'disabled',
     '[required]': 'required',
-    '[attr.name]': 'name || null',
     '[attr.readonly]': 'readonly && !_isNativeSelect || null',
     '[attr.aria-invalid]': 'errorState',
     '[attr.aria-required]': 'required.toString()',
-    '(focus)': '_focusChanged(true)',
-    '(blur)': '_focusChanged(false)',
-    '(input)': '_onInput()',
   },
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [{ provide: XcuFormFieldControl, useExisting: XcuInputComponent }],
 })
 export class XcuInputComponent
@@ -76,19 +61,12 @@ export class XcuInputComponent
   protected _uid = `xcu-input-${nextUniqueId++}`;
   protected _previousNativeValue: any;
   private _inputValueAccessor: { value: any };
-  private _previousPlaceholder: string | null;
-
-  /** Whether the component is being rendered on the server. */
-  readonly _isServer: boolean;
 
   /** Whether the component is a native html select. */
   readonly _isNativeSelect: boolean;
 
   /** Whether the component is a textarea. */
   readonly _isTextarea: boolean;
-
-  /** Whether the input is inside of a form field. */
-  readonly _isInFormField: boolean;
 
   /**
    * Implemented as part of XcuFormFieldControl.
@@ -164,23 +142,12 @@ export class XcuInputComponent
   public placeholder: string;
 
   /**
-   * Name of the input.
-   * @docs-private
-   */
-  @Input()
-  public name: string;
-
-  /**
    * Implemented as part of XcuFormFieldControl.
    * @docs-private
    */
   @Input()
   public get required(): boolean {
-    return (
-      this._required ??
-      this.ngControl?.control?.hasValidator(Validators.required) ??
-      false
-    );
+    return this._required;
   }
   public set required(value: boolean) {
     this._required = coerceBooleanProperty(value);
@@ -273,23 +240,6 @@ export class XcuInputComponent
     return validity && validity.badInput;
   }
 
-  /** Gets the current placeholder of the form field. */
-  protected _getPlaceholder(): string | null {
-    return this.placeholder || null;
-  }
-
-  /** Does some manual dirty checking on the native input `placeholder` attribute. */
-  private _dirtyCheckPlaceholder(): void {
-    const placeholder = this._getPlaceholder();
-    if (placeholder !== this._previousPlaceholder) {
-      const element = this._elementRef.nativeElement;
-      this._previousPlaceholder = placeholder;
-      placeholder
-        ? element.setAttribute('placeholder', placeholder)
-        : element.removeAttribute('placeholder');
-    }
-  }
-
   /**
    * Implemented as part of XcuFormFieldControl.
    * @docs-private
@@ -307,13 +257,10 @@ export class XcuInputComponent
     protected _elementRef: ElementRef<
       HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
     >,
-    protected _platform: Platform,
     @Optional() @Self() public ngControl: NgControl,
     @Optional() _parentForm: NgForm,
     @Optional() _parentFormGroup: FormGroupDirective,
-    public _defaultErrorStateMatcher: ErrorStateMatcher,
-    public ngZone: NgZone,
-    @Optional() @Inject(XCU_FORM_FIELD) public _formField: XcuFormFieldComponent
+    public _defaultErrorStateMatcher: ErrorStateMatcher
   ) {
     super(_defaultErrorStateMatcher, _parentForm, _parentFormGroup, ngControl);
 
@@ -326,50 +273,13 @@ export class XcuInputComponent
     // Force setter to be called in case id was not specified.
     this.id = this.id;
 
-    if (_platform.IOS) {
-      ngZone.runOutsideAngular(() => {
-        this._elementRef.nativeElement.addEventListener(
-          'keyup',
-          this._iOSKeyupListener
-        );
-      });
-    }
-
-    this._isServer = !this._platform.isBrowser;
     this._isNativeSelect = nodeName === 'select';
     this._isTextarea = nodeName === 'textarea';
-    this._isInFormField = !!_formField;
 
     if (this._isNativeSelect) {
       this.controlType = (this.element as HTMLSelectElement).multiple
         ? 'xcu-native-select-multiple'
         : 'xcu-native-select';
-    }
-  }
-
-  public ngAfterViewInit(): void {
-    if (this._platform.isBrowser) {
-      // this._autofillMonitor
-      //   .monitor(this._elementRef.nativeElement)
-      //   .subscribe((event) => {
-      //     this.autofilled = event.isAutofilled;
-      //     this.stateChanges.next();
-      //   });
-    }
-  }
-
-  public ngOnChanges(): void {
-    this.stateChanges.next();
-  }
-
-  public ngOnDestroy(): void {
-    this.stateChanges.complete();
-
-    if (this._platform.IOS) {
-      this._elementRef.nativeElement.removeEventListener(
-        'keyup',
-        this._iOSKeyupListener
-      );
     }
   }
 
@@ -379,28 +289,12 @@ export class XcuInputComponent
       // error triggers that we can't subscribe to (e.g. parent form submissions). This means
       // that whatever logic is in here has to be super lean or we risk destroying the performance.
       this.updateErrorState();
-
-      // Since the input isn't a `ControlValueAccessor`, we don't have a good way of knowing when
-      // the disabled state has changed. We can't use the `ngControl.statusChanges`, because it
-      // won't fire if the input is disabled with `emitEvents = false`, despite the input becoming
-      // disabled.
-      if (
-        this.ngControl.disabled !== null &&
-        this.ngControl.disabled !== this.disabled
-      ) {
-        this.disabled = this.ngControl.disabled;
-        this.stateChanges.next();
-      }
     }
 
     // We need to dirty-check the native element's value, because there are some cases where
     // we won't be notified when it changes (e.g. the consumer isn't using forms or they're
     // updating the value using `emitEvent: false`).
     this._dirtyCheckNativeValue();
-
-    // We need to dirty-check and set the placeholder attribute ourselves, because whether it's
-    // present or not depends on a query which is prone to "changed after checked" errors.
-    this._dirtyCheckPlaceholder();
   }
 
   /** Focuses the input. */
@@ -435,41 +329,4 @@ export class XcuInputComponent
       this.focus();
     }
   }
-
-  /** Callback for the cases where the focused state of the input changes. */
-  private _focusChanged(isFocused: boolean): void {
-    if (isFocused !== this.focused) {
-      this.focused = isFocused;
-      this.stateChanges.next();
-    }
-  }
-
-  private _onInput(): void {
-    // This is a noop function and is used to let Angular know whenever the value changes.
-    // Angular will run a new change detection each time the `input` event has been dispatched.
-    // It's necessary that Angular recognizes the value change, because when floatingLabel
-    // is set to false and Angular forms aren't used, the placeholder won't recognize the
-    // value changes and will not disappear.
-    // Listening to the input event wouldn't be necessary when the input is using the
-    // FormsModule or ReactiveFormsModule, because Angular forms also listens to input events.
-  }
-
-  private _iOSKeyupListener = (event: Event): void => {
-    const el = event.target as HTMLInputElement;
-
-    // Note: We specifically check for 0, rather than `!el.selectionStart`, because the two
-    // indicate different things. If the value is 0, it means that the caret is at the start
-    // of the input, whereas a value of `null` means that the input doesn't support
-    // manipulating the selection range. Inputs that don't support setting the selection range
-    // will throw an error so we want to avoid calling `setSelectionRange` on them. See:
-    // https://html.spec.whatwg.org/multipage/input.html#do-not-apply
-    if (!el.value && el.selectionStart === 0 && el.selectionEnd === 0) {
-      // Note: Just setting `0, 0` doesn't fix the issue. Setting
-      // `1, 1` fixes it for the first time that you type text and
-      // then hold delete. Toggling to `1, 1` and then back to
-      // `0, 0` seems to completely fix it.
-      el.setSelectionRange(1, 1);
-      el.setSelectionRange(0, 0);
-    }
-  };
 }
